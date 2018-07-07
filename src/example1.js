5a457cdf51a5440c76cbd3afb01d5a1da7d94bb2
commanderaipa <gundamgundam777@gmail.com>
2018-07-07 09:45:23 +0900

    [modify]  グラフ表示のもとは用意できた.

--- src/example1.js
+++ src/example1.js
@@ -5,9 +5,9 @@ import {Webcam} from './webcam';
 
 let mobilenet;
 let isPredicting = false;
+const labels = ['高坂桐乃', '高坂京介', '黒猫', '新垣あやせ', '田村麻奈実', '来栖加奈子', '沙織・バジーナ'];
 
 function label_to_text(classId) {
-  const labels = ['高坂桐乃', '高坂京介', '黒猫', '新垣あやせ', '田村麻奈実', '来栖加奈子', '沙織・バジーナ'];
   return labels[classId];
 }
 
@@ -15,6 +15,78 @@ function _reshape(img) {
   const img2 = tf.image.resizeBilinear(img, [28,28]);
   return img2.reshape([1, 1, 28, 28]);
 }
+
+function createBarGraph() {
+  var dataSet = []; //棒グラフの各項目の値
+  var width = 300;
+  var height = 200;
+  var elementName = '#bar';
+
+  for (var i = 0; i < labels.length; i++)
+  {
+    dataSet.push(Math.random())
+  }
+
+  //グラフの各値をいい感じのwidthにするための変換関数
+  var x = d3.scaleLinear() //配列の中身が数字 かつ 線形なグラフのとき
+    .domain([0, 1]) //グラフがとりうる値の範囲
+    .rangeRound([80, width + 50])
+  //実際の長さとしての範囲をいくつからいくつにするか。マージンをどの程度取りたいかの指定として使える。
+
+  //グラフのラベル数にもとづいて、項目間の幅をいい感じにするための変換関数
+  var y = d3
+    .scaleBand() //配列の中身がラベルのとき
+    .domain(labels) //配列をそのまま渡す
+    .rangeRound([20, height]); //実際の長さとしての範囲をいくつからいくつにするか
+
+  //xとyは軸の描画でも使う
+
+  //グラフ用のsvg要素に棒グラフの「棒（rect）」を追加する
+  d3
+    .select(elementName) // svg要素を指定
+    .selectAll('rect') // その中のrect要素を抽出（なければ空オブジェクトが返ってくる）
+    .data(dataSet) // 描画元のデータを指定
+    .enter() // selectAllで足りない分の要素を確保する
+    .append('rect'); //確保した要素にrectとして追加する
+
+  //棒グラフの「棒」の部分の描画
+  d3.select(elementName).selectAll('rect') //svg要素の中のrect要素をすべて選択（さっき作ったもの）
+    .attr("class", "bar") //棒グラフであることを明示（必須ではない）
+    .attr("x", function(d){
+      return x(0);
+    })
+    //各値のx座標を指定。左端にしたいので0にする。
+    //ここで、x関数をかませることで、マージンを考慮した「左端」が指定できる
+    .attr("y", function(d, i){
+      return y(labels[i]);
+    })
+    //各要素のy座標をy関数を使って指定。y関数はlabels内の要素に紐付いているので、y(labels)の形式になる。
+    //iはdataSetの何番目の要素を参照しているかが入る
+    .attr("height", 15)
+    //各棒の縦の長さ（今回は棒の幅）
+    .attr("width", function(d){
+      return x(d);
+    })
+    //各棒の横の長さ（今回は棒の長さ）
+    //dはdataSetの値そのものが入る。x関数を噛ませて長さを指定。
+    .attr("fill", "#6fbadd");
+    //棒の色
+
+  //棒グラフのx軸の描画
+  d3.select(elementName) //グラフ用のsvg要素を持ってくる
+  .append("g") //軸描画用の要素を追加（gという名前じゃないと動かない）
+  .attr("class", "axis axis--x") //x軸用の要素を追加
+  .attr("transform", "translate(0, 20)") //(0,20)の位置を起点として描画
+  .call(d3.axisTop(x)); //上に軸が出るように設定
+
+　//棒グラフのy軸の描画
+  d3.select(elementName) //グラフ用のsvg要素を持ってくる
+  .append("g") //軸描画用の要素を追加（gという名前じゃないと動かない）
+  .attr("class", "axis axis--y") //y軸用の要素を追加
+  .attr("transform", "translate(80, 0)") //(80,0)の位置を起点として描画
+  .call(d3.axisLeft(y)); //左に軸が出るように設定
+}
+
 
 document.addEventListener("DOMContentLoaded", function() {
   const webcam = new Webcam(document.getElementById('webcam'));
@@ -27,7 +99,8 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   async function loadModel() {
-    tf.loadModel('https://s3.us-east-2.amazonaws.com/tfjs-demo/mobilenet/model.json')
+    // tf.loadModel('https://s3.us-east-2.amazonaws.com/tfjs-demo/mobilenet/model.json')
+    tf.loadModel('https://localhost:8000/mobilenet/model.json')
       .then(model => {
       const m = tf.model({inputs: model.inputs, outputs: model.outputs});
       const img = webcam.capture();
@@ -62,6 +135,7 @@ document.addEventListener("DOMContentLoaded", function() {
       const classId = (await predictedClass.data());
       const label = label_to_text(classId[0])
       document.getElementById('display').innerText = label;
+      createBarGraph();
       return classId;
     }
 
